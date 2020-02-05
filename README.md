6289547b396f3444bfb2a8345406a3855f4ed425
aman <man.alex1995@gmail.com>
2020-02-04 23:01:29 -0800

    need to create tables and move parsed info into db

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # finance-site
 Monitor spendings and track bills
 
+# Dev-platform
+Ubuntu 16.04
 
-python 2.7.12
+##############
+# Requirements
+##############
+# python
+python 3.5.2
+
+# mysql
+sudo apt install mysql-server
+
+# pandas
+sudo pip install pandas
+
+# mysql-connector-python
+sudo pip install mysql-connector-python
--- data_retrieval.py
+++ data_retrieval.py
@@ -6,16 +6,41 @@ Intended for moving/storing transaction csv into database
 Alex Man
 """
 
-import os
-import sys
+import pandas as pd
 from pprint import pprint
+import mysql.connector
+import sys
+import os
 
+"""
+Need to setup the database on new environments
+"""
+
+datafile = os.path.expanduser('~/finance-site/download.csv')
 
 if len(sys.argv) < 2:
 	print("python data_retrieval.py [csv-file]")
 	exit()
 
 
-with open(sys.argv[1], 'r') as infile:
-	contents = infile.read()
-	print(contents)
+df = pd.read_csv(datafile)
+# pprint(df)
+
+conn = mysql.connector.connect(
+	user		='root',
+	password	='root',
+	host		='localhost')
+
+
+cursor = conn.cursor()
+
+try:
+	statement = "create database transactions;"
+	cursor.execute(statement)
+except Exception as err:
+	print('Database already exists: ', err)
+
+
+
+conn.close()
+
